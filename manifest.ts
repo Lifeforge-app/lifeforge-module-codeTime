2b3fa7427c0fc6e36ef37ba24b8ac06859e462fd
Melvin Chia <melvinchia623600@gmail.com>
2026-01-05 11:59:11 +0800

    feat: update to latest version from forgistry

--- manifest.ts
+++ manifest.ts
@@ -2,10 +2,7 @@ import { lazy } from 'react'
 import type { ModuleConfig } from 'shared'
 
 export default {
-  name: 'Code Time',
-  icon: 'tabler:code',
   routes: {
     '/': lazy(() => import('@'))
   },
-  category: 'Productivity'
 } satisfies ModuleConfig
